a7d5445a9d5fbf78d447ec5b11099c4f8bd1bb80
Joshua Chapman <joshua.chapman@joshuachp.dev>
2026-02-20 06:53:54 +0000

    mctl: add 0.2.2 bottle.

--- Formula/mctl.rb
+++ Formula/mctl.rb
@@ -5,6 +5,12 @@ class Mctl < Formula
   sha256 "c830deae4a3e5ac9e8c135c4d60a0d75cac5222c172a15f8d087e1a25b8d99c2"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/joshuachp/homebrew-tap/releases/download/mctl-0.2.2"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "e7eee89fe2c9d9fd001435024d9b2e018e7534207c9a4be25e69a2a955aa36d2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "713e23e546f518bfeebba37df0204aba3e3ed4fd80fecb915bc4a7b07cbd3f5a"
+  end
+
   depends_on "rust" => :build
 
   def install
